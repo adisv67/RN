da9bd07c99c34e2ce07736ecd5dab295199a38f0
adesh <adesh.sonawane@morfsys.com>
2024-06-14 11:41:18 +0530

    jsx started

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1", {id:"random"},"abcd");
-const root  = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+
+const JSXheading = (<h1 className = "random" tabIndex= "1">
+    Namste Adesh
+    </h1>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(JSXheading);
